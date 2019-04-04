b9fc812200de7d31692cc17ce3fe8ef0735605cd
ViniciusMizani <30930359+ViniciusMizani@users.noreply.github.com>
2019-04-04 09:10:55 -0300

    .

--- celula.cs
+++ celula.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace MatrizEsparsa
 {
-    class celula
+    class Celula
     {
+        private double valor;
+        private int linha, coluna;
+        private Celula direita, abaixo;
+
+        public Celula(double valor, int linha, int coluna, Celula direita, Celula abaixo)
+        {
+            this.valor   = valor;
+            this.linha   = linha;
+            this.coluna  = coluna;
+            this.direita = direita;
+            this.abaixo  = abaixo;
+            
+        }
+
+        public double Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public int Linha
+        {
+            get { return linha; }
+            set { linha = value; }
+        }
+
+        public int Coluna
+        {
+            get { return coluna; }
+            set { coluna = value; }
+        }
+
+        public Celula Direita
+        {
+            get { return direita; }
+            set { direita = value; }
+        }
+
+        public Celula Abaixo
+        {
+            get { return abaixo; }
+            set { abaixo = value; }
+        }
+
+        public override string ToString()
+        {
+            String ret = "(";
+            if (this.valor == null)
+                this.valor = 0;
+
+            ret += this.Valor + ";" + this.Linha + ";" + this.Coluna;
+
+            return ret + ")";
+        }
     }
+
+    
 }
